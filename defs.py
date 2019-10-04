53ba5da4ac06f1579ef4a5fc1ff66ae3f7888cde
Vicrobot <chaosplusorder@gmail.com>
2019-10-04 14:02:45 -0400

    Initial Commmit

---
+++ defs.py
@@ -0,0 +1,198 @@
+import flickrapi
+import flickr_api
+import urllib.request
+import time, magic
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PIL import Image
+from PIL.ImageQt import ImageQt
+import os, sys, time
+
+
+def url_list_maker(flickr_obj, uiv):
+    """
+    Returns a list of urls of all public photos a user has uploaded on flickr.
+    User is identified through user id value(uiv) in argument.
+    """
+    count = 0
+    photos = flickr_obj.walk_user(user_id = uiv, per_page = 100, extras = 'url_o')
+    url_list = []
+    for photo in photos:
+        try:
+            url_list.append(photo.get('url_o')) # o ->original size; other vars may not have all images.
+        except: pass
+    return url_list
+
+def mkname(name):
+    """
+    Returns a possible non-conflicting name able to be created in current directory.
+    """
+    num = 0
+    name = str(name)
+    new_n = name[:]
+    old_n = new_n
+    while os.path.exists(new_n):
+        old_n = new_n
+        num += 1
+        new_n = name + str(num)
+    return(new_n, old_n)
+
+def checkIds(akv, skv, print_M = 0):
+    """
+    Checks whether the provided keys are correct or not.
+    akv is access key, akv is secret key.
+    print_M is used for showing a message if keys were wrong.
+    """
+    flickr_api.set_keys(api_key = akv, api_secret = skv)
+    try: flickr_api.Person.findByUserName('vicro_bot').id
+    except flickr_api.flickrerrors.FlickrAPIError:
+        if print_M: anim_write("Wrong Keys!!", "Try again")
+        return 0
+    return 1
+
+def anim_write(*string, t = 0.02):
+    "AnimationWriter:- Prints iterable in fashion"
+    for i in string:
+        for j in i:
+            print(j, end = '', flush = True)
+            time.sleep(t)
+        print('')
+        time.sleep(0.02)
+
+def input_anim(string, t=0.05):
+    anim_write(string)
+    return input()
+
+def is_img(st):
+    if magic.from_file(var + '/'  +  st, mime = 1).startswith('image'): return True
+    else: return False
+
+def download(urls, filename, progressBarObj, imagepreview, choice = 0, imagecount = 0):
+    print('i am here', '{} urls are here'.format(len(urls)), filename, progressBarObj, sep = '\n')
+    """
+    Downloads files from url and uses filename as starting name for files.
+    choice : it is the way of accessing photos(like through tags, user name etc).
+    """
+    pgo = progressBarObj
+    if choice == 1:
+        with open('.temp-logs', 'r+') as var: t_lines = var.readlines()
+    counter, b = 0, 0
+    k1 = len(urls)
+    if k1 == 0:
+        return 0
+    var = 100.0/(k1*1.0)
+    for i in urls:
+        imagename = '{1}{0}.{2}'.format(imagecount, filename[0], i.split('.')[-1])
+        try: urllib.request.urlretrieve( i, imagename)
+        except KeyboardInterrupt:
+            if choice == 1:
+                #only for choice == 1 since no need to store urls for search results as they vary.
+                with open('.temp-logs', 'w+') as var:
+                    var.write(t_lines[0])
+                    var.write('imgC {}'.format(imagecount))
+                print('\nAbort')
+                sys.exit()
+            else: print('\nAbort'); sys.exit()
+        counter += var
+        if is_img(imagename):
+            preview(imagename, imagepreview)
+        imagecount += 1
+        pgo.setProperty("value", float('{:05}'.format(counter)[:5]))
+    return 1
+
+def preview(im, imprev):
+    t = Image.open(im)
+    img = t.resize((251, 321), Image.ANTIALIAS)
+    pix = QtGui.QPixmap.fromImage(QtGui.QImage(ImageQt(img)))
+    v1 = QtWidgets.QGraphicsScene(self.centralwidget)
+    v1.addPixmap(pix)
+    imprev.setScene(v1)
+    #self.graphicsView.addPixmap(pix)
+
+
+def searchnDownload(str1, typ, pgo, imprev, to_search):
+    print(' i am here', str1, typ, pgo, sep = '\n')
+    """ pgo is progress bar 
+        imprev is image preview place
+    """
+    if typ.lower() == 'tags':
+        tags = [i.rstrip() for i in str1.split(',') if i]
+        #bool_broad = int(input_anim('You wanna search broad category or strict in tagging?\
+        #(1 for former/prior, 0 for later):').rstrip())
+        #text = input_anim("Give a general text for search: ").strip()
+    
+        if bool_broad == 1:
+            t = flickr.tags.getRelated(api_key = api_key_val, tag = tags)
+            tags = [[j.text for j in i] for i in t][0]
+            if isinstance(to_search, None):
+                searched_elem = flickr.photos.search(api_key = api_key_val, tags = tags, 
+            text = text, accuracy = 1, safe_search = 1, content_type = 1, extras = 'url_o',
+            per_page = 10)#int(input_anim('how many images(max 500): ').rstrip())) 
+            #there is media argument, per_page and page too. GUI handling needed.
+            elif to_search == 'images' or to_search == 'others':
+                searched_elem = flickr.photos.search(api_key = api_key_val, tags = tags, 
+            text = text, accuracy = 1, safe_search = 1, content_type = 1, extras = 'url_o',
+            per_page = 10, media = to_search)
+        else:
+            if isinstance(to_search, None):
+                searched_elem = flickr.photos.search(api_key = api_key_val, tags = tags,
+            text = text, accuracy = 1, safe_search = 1, content_type = 1, extras = 'url_o', 
+            per_page = 10)#int(input_anim('how many images(max 500): ').rstrip()))
+            elif to_search == 'images' or to_search == 'others':
+                searched_elem = flickr.photos.search(api_key = api_key_val, tags = tags,
+            text = text, accuracy = 1, safe_search = 1, content_type = 1, extras = 'url_o', 
+            per_page = 10, media = to_search)
+        photo_elems = [[j for j in i] for i in searched_elem][0]
+        url_list = []
+        counter_photo, printed, len_p = 1, False, 0
+        for p in photo_elems:
+            try:
+                dict_ = p.attrib
+                md = flickr.photos.getSizes(api_key = api_key_val, photo_id = dict_['id'])
+                t1 = [[j.attrib['source'] for j in i][-1] for i in md][0]
+                url_list.append(t1)
+            except KeyboardInterrupt: 
+                print('\nAbort')
+                sys.exit()
+            except:
+                printed = False
+                anim_write('Error occured in retrieving url, Ignoring')
+        #directory work
+        new_dir, old_dir = mkname('Flickr_Imgs_{}'.format('_'.join(text.split(' '))))
+        if not os.path.exists(old_dir):
+            os.mkdir(new_dir)
+            os.chdir(new_dir)
+        else: os.chdir(old_dir)
+        downloaded = download(url_list, text, choice = 0)
+        return downloaded
+        #do stuffs
+    elif typ.lower() == 'name':
+        user_name = ''.join([i.rstrip() for i in str1.split(',') if i])
+        #do stuffs
+        user_id_val = flickr_api.Person.findByUserName(user_name).id
+        #directory work
+        new_dir, old_dir = mkname('Flickr_Imgs_{}'.format('_'.join(user_name.split(' '))))
+        if not os.path.exists(old_dir):
+            os.mkdir(new_dir)
+            os.chdir(new_dir)
+        else: os.chdir(old_dir)
+
+        imagecount = 0
+        if not os.path.exists('.temp-logs'):
+            with open('.temp-logs', 'w+') as var:
+                urls = url_list_maker(flickr, user_id_val)
+                var.write(str(urls))
+                var.write('\n')
+        else:
+            with open('.temp-logs', 'r+') as var:
+                lines_urls_lgs = var.readlines()
+            try: susp_int= lines_urls_lgs[1].rstrip()[5:]
+            except IndexError:
+                with open('.temp-logs', 'w+') as var:
+                    urls = url_list_maker(flickr, user_id_val)
+                    var.write(str(urls))
+                    var.write('\n')
+                    susp_int = 0
+            if susp_int: imagecount = int(susp_int) -1
+            urls = eval(lines_urls_lgs[0].rstrip())[imagecount :]
+        downloaded = download(urls, user_name, pgo, imprev, choice = 1, imagecount = imagecount)
+    return downloaded
---
+++ flickr.py
@@ -0,0 +1,195 @@
+# -*- coding: utf-8 -*-
+
+from defs import *
+
+"""
+To dos:-
+
+Folder choice.
+broad search, or narrow in tags
+
+per_page images, fading of image and video checkbox when user_name radio button is clicked.
+"""
+
+class Ui_FlickrDownloader(object):
+    def setupUi(self, FlickrDownloader):
+        FlickrDownloader.setObjectName("FlickrDownloader")
+        FlickrDownloader.resize(815, 400)
+        font = QtGui.QFont()
+        font.setFamily("Ubuntu")
+        font.setPointSize(12)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
+        FlickrDownloader.setFont(font)
+        self.centralwidget = QtWidgets.QWidget(FlickrDownloader)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(26, 70, 91, 20))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(26, 100, 111, 20))
+        self.label_2.setObjectName("label_2")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(160, 70, 161, 23))
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(160, 100, 161, 23))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(30, 170, 481, 61))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.radioButton = QtWidgets.QRadioButton(self.horizontalLayoutWidget)
+        self.radioButton.setObjectName("radioButton")
+        self.horizontalLayout.addWidget(self.radioButton)
+        self.radioButton_2 = QtWidgets.QRadioButton(self.horizontalLayoutWidget)
+        self.radioButton_2.setObjectName("radioButton_2")
+        self.horizontalLayout.addWidget(self.radioButton_2)
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_3.setGeometry(QtCore.QRect(32, 242, 471, 31))
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.checkBox = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBox.setGeometry(QtCore.QRect(340, 280, 85, 21))
+        self.checkBox.setObjectName("checkBox")
+        self.checkBox_2 = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBox_2.setGeometry(QtCore.QRect(340, 310, 211, 21))
+        self.checkBox_2.setObjectName("checkBox_2")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(30, 280, 80, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.checkBox_3 = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBox_3.setGeometry(QtCore.QRect(330, 80, 211, 31))
+        self.checkBox_3.setObjectName("checkBox_3")
+        self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar.setGeometry(QtCore.QRect(30, 310, 118, 23))
+        self.progressBar.setProperty("value", 0)
+        self.progressBar.setObjectName("progressBar")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(600, 10, 141, 20))
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(210, 20, 191, 31))
+        self.label_4.setText("")
+        self.label_4.setObjectName("label_4")
+        self.graphicsView = QtWidgets.QGraphicsView(self.centralwidget)
+        self.graphicsView.setGeometry(QtCore.QRect(540, 40, 251, 321))
+        self.graphicsView.setObjectName("graphicsView")
+        FlickrDownloader.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(FlickrDownloader)
+        self.statusbar.setObjectName("statusbar")
+        FlickrDownloader.setStatusBar(self.statusbar)
+
+        self.retranslateUi(FlickrDownloader)
+        QtCore.QMetaObject.connectSlotsByName(FlickrDownloader)
+        self.pushButton.clicked.connect(self.go)
+        self.checkBox_3.clicked.connect(self.cb3)
+        self.lineEdit_3.setText('search here (comma separated)')
+        if not os.path.exists('logs'): self.checkBox_3.setDisabled(True)
+        else:
+            self.api_set = False
+            try:
+                with open('logs', 'r') as  var:
+                    self.lines = [i.rstrip() for i in var.readlines() if len(i) ]
+            except FileNotFoundError:
+                with open('logs', 'w+') as  var:
+                    self.lines = []
+            self.dict_ids = {}
+            #ids_handeling
+            id1_set, id2_set = 0, 0
+            for line in self.lines:
+                if 'id1' in line: 
+                    self.dict_ids['id1'] = ''.join(line.split(' ')[1:])
+                    id1_set = 1
+                if 'id2' in line:
+                    self.dict_ids['id2'] = ''.join(line.split(' ')[1:])
+                    id2_set = 1
+            if not id1_set or not id2_set: self.checkBox_3.setDisabled(True)
+        self.work_being_done = False
+    def retranslateUi(self, FlickrDownloader):
+        _translate = QtCore.QCoreApplication.translate
+        FlickrDownloader.setWindowTitle(_translate("FlickrDownloader", "MainWindow"))
+        self.label.setText(_translate("FlickrDownloader", "API Key"))
+        self.label_2.setText(_translate("FlickrDownloader", "API Secret Key"))
+        self.radioButton.setText(_translate("FlickrDownloader", "Search by user name"))
+        self.radioButton_2.setText(_translate("FlickrDownloader", " Search by tags"))
+        self.checkBox.setText(_translate("FlickrDownloader", "Images"))
+        self.checkBox_2.setText(_translate("FlickrDownloader", "Other medias like videos"))
+        self.pushButton.setText(_translate("FlickrDownloader", "Go"))
+        self.checkBox_3.setText(_translate("FlickrDownloader", "Use previously used keys"))
+        self.label_3.setText(_translate("FlickrDownloader", "Preview of image"))
+    def cb3(self):
+        if self.checkBox_3.checkState() == 2 and self.api_set == False:
+            self.lineEdit.setText(self.dict_ids['id1'])
+            self.lineEdit_2.setText(self.dict_ids['id2'])
+            self.api_set = True
+    def go(self):
+        if self.work_being_done: return
+        if not self.radioButton.isChecked() and not self.radioButton_2.isChecked():
+            #message here
+            print('press radio buttons')
+            return
+        if self.checkBox_3.checkState() == 0 and not self.lineEdit.text().strip() + self.lineEdit_2.text().strip():
+            #message Here
+            print('give valid ids')
+            return
+        if not self.lineEdit_3.text().strip():
+            #message Here
+            print('give search terms')
+            return
+        if self.checkBox.checkState() == 0 and self.checkBox_2.checkState() == 0:
+            #message here
+            print(' please select type of media to download ')
+            return
+        if self.checkBox_3.checkState() == 2:
+            self.id1 = self.dict_ids['id1']
+            self.id2 = self.dict_ids['id2']
+            self.lineEdit.setText(self.id1)
+            self.lineEdit_2.setText(self.id2)
+        else:
+            self.id1 = self.lineEdit.text()
+            self.id2 = self.lineEdit_2.text()
+        if not checkIds(self.id1, self.id2):
+            self.lineEdit.clear()
+            self.lineEdit_2.clear()
+            self.checkBox_3.setChecked(False)
+            self.api_set = False
+            self.label_4.setText('Wrong Keys!! Try Again')
+            return
+        else:
+            with open('logs', 'w+') as var:
+                var.writelines(['id1 {}\n'.format(self.id1), 'id2 {}'.format(self.id2)])
+                self.tag = self.radioButton_2.isChecked()
+                self.name = self.radioButton.isChecked()
+                self.work_being_done = True
+                self.label_4.setText('Processing...')
+                global flickr
+                flickr=flickrapi.FlickrAPI(self.id1, self.id2)
+                flickr_api.set_keys(api_key = self.id1, api_secret = self.id2)
+                if self.checkBox.checkState() == 2 and self.checkBox_2.checkState() == 2:
+                    search_this = 'both'
+                elif self.checkBox.checkState() == 2 and self.checkBox_2.checkState() == 0:
+                    search_this = 'images'
+                elif self.checkBox.checkState() == 0 and self.checkBox_2.checkState() == 2:
+                    search_this = 'others'
+                if self.tag:
+                    k = searchnDownload(self.lineEdit_3.text(), 'tags', self.progressBar, search_this)
+                else:
+                    k = searchnDownload(self.lineEdit_3.text(), 'name', self.progressBar, None)
+                if k == 1: self.label_4.setText('Images Downloaded')
+                elif k == 0: self.label_4.setText('No images found')
+        """
+        Scenes are visualized by graphicsView sort of objs.
+        Size of graphics view:- (540, 40, 251, 321) (x, y, width, height)
+        """
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    FlickrDownloader = QtWidgets.QMainWindow()
+    ui = Ui_FlickrDownloader()
+    ui.setupUi(FlickrDownloader)
+    FlickrDownloader.show()
+    sys.exit(app.exec_())
